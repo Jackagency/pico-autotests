c4cdc0709f56be54df93c9df512b7631b8753173
jackagency <Diablo372@yandex.ru>
2022-04-21 13:47:34 +0300

    fixed userEdit test

--- src/test/java/pages/UserPageComponents.java
+++ src/test/java/pages/UserPageComponents.java
@@ -13,7 +13,7 @@ public class UserPageComponents {
 
     //авторизация
     @Step("Проходим авторизацию")
-    public UserPageComponents authorizeSupd(String login, String password) {
+    public void authorizeSupd(String login, String password) {
 
         SelenideElement loginInput = $("[data-id=login_login]");
         SelenideElement passwordInput = $("[data-id=login_password]");
@@ -22,20 +22,16 @@ public class UserPageComponents {
         loginInput.setValue(login).pressEnter();
         passwordInput.setValue(password).pressEnter();
         submitButton.click();
-
-        return this;
-
     }
 
     @Step("Открываем браузер")
-    public UserPageComponents openLoginPage() {
+    public void openLoginPage() {
         open("login");
-        return this;
     }
 
     //основание
     @Step("Заполняем поля основания")
-    public UserPageComponents reasonForm(String reason, String type, String number, String name, String date) {
+    public void reasonForm(String reason, String type, String number, String name, String date) {
 
         SelenideElement userReasonInput = $("[data-id=reason_form_reason]");
         SelenideElement userReasonTypeInput = $("[data-id=reason_form_type]");
@@ -48,13 +44,10 @@ public class UserPageComponents {
         userReasonNumberInput.scrollIntoView(false).setValue(number);
         userAgreementNameInput.scrollIntoView(false).setValue(name);
         userAgreementDate.scrollIntoView(false).setValue(date);
-
-        return this;
-
     }
 
     @Step("Сверяем информацию о созданном пользователе")
-    public UserPageComponents userInfoCheck(String surname, String name, String patronymic, String login, String adlogin, String kadrid, String email, String isactive, String type) {
+    public void userInfoCheck(String surname, String name, String patronymic, String login, String adlogin, String kadrid, String email, String isactive, String type) {
 
         SelenideElement userInfoSurname = $("#user_info_surname");
         SelenideElement userInfoName = $("#user_info_name");
@@ -75,142 +68,118 @@ public class UserPageComponents {
         userInfoEmail.scrollIntoView(false).shouldHave(text(email));
         userInfoActive.scrollIntoView(false).shouldHave(text(isactive));
         userInfoAccType.scrollIntoView(false).shouldHave(text(type));
-
-        return this;
-
     }
 
     @Step("Нажимаем кнопку Создать")
-    public UserPageComponents userCreateButton() {
+    public void userCreateButton() {
         SelenideElement userCreateButton = $("#user_create_btn");
         userCreateButton.shouldBe(visible).click();
-        return this;
     }
     @Step("Нажимаем кнопку Подтвредить")
-    public UserPageComponents userCreateSubmitButton() {
+    public void userCreateSubmitButton() {
         SelenideElement userCreateSubmitButton = $("#user_edit_submit");
         SelenideElement toolbarText = $("div.dx-toolbar-before div.dx-item-content div");
         userCreateSubmitButton.scrollIntoView(false).shouldBe(visible).click();
         userCreateSubmitButton.shouldBe(not(visible), Duration.ofMillis(15000));
         toolbarText.shouldBe(Condition.not(Condition.visible), Duration.ofMillis(15000));
-
-        return this;
     }
 
     @Step("Нажимаем кнопку Подтвердит")
-    public UserPageComponents userDeleteSubmitButton() {
+    public void userDeleteSubmitButton() {
         SelenideElement userDeleteSubmitButton = $("[data-id=confirm_submit]");
         SelenideElement toolbarText = $("div.dx-toolbar-before div.dx-item-content div");
         userDeleteSubmitButton.scrollIntoView(false).shouldBe(visible).click();
         userDeleteSubmitButton.shouldBe(not(visible), Duration.ofMillis(15000));
         toolbarText.shouldBe(Condition.not(Condition.visible), Duration.ofMillis(15000));
-        return this;
     }
     @Step("Нажимаем кнопку информация о пользователе")
-    public UserPageComponents clickInfoButton() {
+    public void clickInfoButton() {
         SelenideElement userInfoButton = $("[data-id=user_show_info]");
         userInfoButton.scrollIntoView(false).shouldBe(visible).click();
-        return this;
     }
     @Step("Открываем выпадающий список действий с пользователем")
     public UserPageComponents clickActionButton() {
         SelenideElement userActionButton = $("[data-id=actions_btn]");
-        userActionButton.scrollIntoView(false).shouldBe(visible).click();
+        userActionButton.scrollIntoView(true).shouldBe(visible).click();
         return this;
     }
     @Step("Выбираем действие Редактировать")
-    public UserPageComponents clickUserEditButton() {
+    public void clickUserEditButton() {
         SelenideElement userUserEditButton = $("[data-id=actions_btn_edit]");
         userUserEditButton.shouldBe(visible).click();
-        return this;
     }
     @Step("Выбираем действие удалить")
-    public UserPageComponents clickUserDeleteButton() {
+    public void clickUserDeleteButton() {
         SelenideElement userUserDeleteButton = $("[data-id=actions_btn_delete]");
         userUserDeleteButton.shouldBe(visible).click();
-        return this;
     }
     @Step("Прокрутка таблицы влево")
-    public UserPageComponents scrollUserTableTobegin() {
+    public void scrollUserTableTobegin() {
         SelenideElement userSurnameTable = $("#dx-col-101");
         userSurnameTable.scrollIntoView(true).shouldHave(text("Фамилия"));
-        return this;
     }
 
     @Step("Проверяем что удаленного пользователя нет в списке пользователей")
-    public UserPageComponents emptyTableCheck() {
+    public void emptyTableCheck() {
         SelenideElement emptyTableSign = $(".dx-datagrid-nodata");
         emptyTableSign.shouldBe(visible).shouldHave(text("Нет данных"));
-        return this;
-
     }
 
     @Step("Выбираем чек-бокс Эксперт")
-    public UserPageComponents setExpertCheckbox() {
+    public void setExpertCheckbox() {
         SelenideElement expertCheckBox = $("#user_account_type :nth-child(1) .dx-radio-value-container");
         expertCheckBox.shouldBe(visible).click();
-        return this;
     }
 
     @Step("Выбираем чек-бокс Служебный сервис")
-    public UserPageComponents setServiceCheckbox() {
+    public void setServiceCheckbox() {
         SelenideElement serviceCheckbox = $("#user_account_type :nth-child(2) > div.dx-radio-value-container");
         serviceCheckbox.shouldBe(visible).click();
-        return this;
     }
 
     @Step("Выбираем чек-бокс Временный")
-    public UserPageComponents setTemporaryCheckbox() {
+    public void setTemporaryCheckbox() {
         SelenideElement temporaryCheckbox = $("#user_temporary");
         temporaryCheckbox.shouldBe(visible).click();
-        return this;
     }
 
     @Step("Проверяем сообщения о некорректной дате временного пользователя")
-    public UserPageComponents wrongTemporaryDateChek() {
+    public void wrongTemporaryDateChek() {
         SelenideElement wrongTemporaryDateStart = $("div.mr-2 .dx-invalid-message-content");
         SelenideElement wrongTemporaryDateEnd = $("div:nth-child(2) > div > .dx-invalid > .dx-invalid-message");
         wrongTemporaryDateStart.shouldBe(visible).shouldHave(text("Дата не может быть раньше текущей"));
         wrongTemporaryDateEnd.shouldBe(visible).shouldHave(text("Дата не может быть раньше текущей или раньше «даты от»"));
-        return this;
     }
     @Step("Нажимаем чек-бокс пользователя")
-    public UserPageComponents clickUserCheckbox(){
+    public void clickUserCheckbox(){
         SelenideElement userCheckbox = $(".dx-select-checkbox", 1); //кликает первый чек-бокс во всем списке
         userCheckbox.shouldBe(visible).click();
-        return this;
     }
     @Step("Нажимаем кнопку Назначить временные права")
-    public UserPageComponents clickTemporaryRightsButton(){
+    public void clickTemporaryRightsButton(){
         SelenideElement temporaryRightsButton = $x(".//button[contains(.,'Назначить временные права')]");
         temporaryRightsButton.shouldBe(visible).click();
-        return this;
     }
     @Step("Выбираем первый чек-бокс в списке")
-    public UserPageComponents clickObjectsCheckbox(){
+    public void clickObjectsCheckbox(){
         SelenideElement objectCheckbox = $(".app-user-access-form__table .dx-datagrid-rowsview table tr:nth-child(1) td.dx-command-select > div");
         objectCheckbox.shouldBe(visible).click();
-        return this;
     }
     @Step("Нажимаем кнопку Применить")
-    public UserPageComponents clickSubmitButton(){
+    public void clickSubmitButton(){
         SelenideElement submitButton = $x(".//button[contains(.,'Применить')]");
         SelenideElement successMessage = $(".notices.is-bottom > div");
         submitButton.shouldBe(visible).click();
-        successMessage.shouldBe(visible).shouldHave(text("Временные права назначены"));
-        return this;
     }
     @Step("Проверяем первую строчку в таблице групп")
-    public UserPageComponents firstLineGroupTableCheck(){
+    public void firstLineGroupTableCheck(){
         SelenideElement firstLine = $("#groups tr.dx-row.dx-data-row.dx-column-lines td:nth-child(2)");
         firstLine.shouldHave(text("TEMP_GROUP"));
-        return this;
     }
     @Step("Нажимаем на первую строчку в таблице групп")
-    public UserPageComponents firstLineGroupTableClick(){
+    public void firstLineGroupTableClick(){
         SelenideElement firstLine = $("#groups tr.dx-row.dx-data-row.dx-column-lines td:nth-child(2)");
         firstLine.click();
-        return this;
     }
 
 }
